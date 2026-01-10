6fb77b3cb55dfade927b2b643aa75678a722f611
Adam <adam@faker.com>
2026-01-09 19:31:50 -0800

    Removed Archive

--- archive/generate_event.py
+++
@@ -1,7 +0,0 @@
-from happysimulator.events.event import Event
-from happysimulator.utils.instant import Instant
-
-
-class GenerateEvent(Event):
-    def __init__(self, time: Instant, callback, name: str):
-        super().__init__(time, name, callback)

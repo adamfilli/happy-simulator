b8a97f45970af27ed09040b7a196771ea2ab6124
Adam <adam@faker.com>
2026-01-30 21:26:41 -0800

    Add Network

--- happysimulator/components/network/__init__.py
+++ happysimulator/components/network/__init__.py
@@ -5,5 +5,6 @@ latency, bandwidth constraints, packet loss, and network topologies.
 """
 
 from happysimulator.components.network.link import NetworkLink
+from happysimulator.components.network.network import Network
 
-__all__ = ["NetworkLink"]
+__all__ = ["NetworkLink", "Network"]

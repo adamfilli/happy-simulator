3610e9243d11df2ba93e9af1cab4f52b116daa30
Adam <adam@faker.com>
2026-01-30 21:41:04 -0800

    Add Network topology tests

--- happysimulator/components/network/__init__.py
+++ happysimulator/components/network/__init__.py
@@ -6,5 +6,28 @@ latency, bandwidth constraints, packet loss, and network topologies.
 
 from happysimulator.components.network.link import NetworkLink
 from happysimulator.components.network.network import Network
+from happysimulator.components.network.conditions import (
+    local_network,
+    datacenter_network,
+    cross_region_network,
+    internet_network,
+    satellite_network,
+    lossy_network,
+    slow_network,
+    mobile_3g_network,
+    mobile_4g_network,
+)
 
-__all__ = ["NetworkLink", "Network"]
+__all__ = [
+    "NetworkLink",
+    "Network",
+    "local_network",
+    "datacenter_network",
+    "cross_region_network",
+    "internet_network",
+    "satellite_network",
+    "lossy_network",
+    "slow_network",
+    "mobile_3g_network",
+    "mobile_4g_network",
+]
